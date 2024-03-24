4ebbd61cc4a13eacc6b2d871786a8afce03894d1
wangbin <wangbinim@outlook.com>
2024-03-25 00:43:11 +0800

    added log to Values and update nlog.config

---
+++ src/FinanceTracker.Service/Controllers/ValuesController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FinanceTracker.Service.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly ILogger<ValuesController> logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            this.logger = logger;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> TaskAsync()
+        {
+            await Task.Delay(500);
+            return Ok();
+        }
+    }
+}
