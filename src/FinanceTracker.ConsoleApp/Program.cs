d61abefc7d60b1a0aa391eb93bc5ea88cd5d1ba4
wangbin <wangbinim@outlook.com>
2025-02-17 11:37:56 +0800

    add ConsoleApp

---
+++ src/FinanceTracker.ConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using FinanceTracker.ConsoleApp.Worker;
+
+namespace FinanceTracker.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            ExceptionCaptureDebugging data = new ();
+            data.Run();
+            Console.WriteLine("Main");
+        }
+    }
+}
---
+++ src/FinanceTracker.ConsoleApp/Worker/ExceptionCaptureDebugging.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanceTracker.ConsoleApp.Worker
+{
+    internal class ExceptionCaptureDebugging
+    {
+        public void Run()
+        {
+			try
+			{
+				Console.WriteLine("Start");
+			}
+			catch (Exception)
+			{
+
+				throw;
+			}
+			finally
+			{
+				Console.WriteLine("End");
+			}
+        }
+    }
+}
